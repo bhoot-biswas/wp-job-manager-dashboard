b2027750576a143bae2840d84c95b1a791c61f28
Mithun Biswas <bhoot.biswas@gmail.com>
2020-11-04 22:31:22 +0600

    Add initial files

---
+++ wp-job-manager-dashboard.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Wp Job Manager Dashboard
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     wp-job-manager-dashboard
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wp_Job_Manager_Dashboard
+ */
+
+// Your code starts here.
